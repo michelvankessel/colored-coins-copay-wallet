34cf3693ec3874409abb72056362831495da1451
janko33bd <janko33bd@gmail.com>
2018-01-06 20:23:46 +0100

    units fix

--- src/js/controllers/preferencesUnit.js
+++ src/js/controllers/preferencesUnit.js
@@ -8,15 +8,15 @@ angular.module('copayApp.controllers').controller('preferencesUnitController', f
 
   $scope.unitList = [
     {
-      name: 'bits (1,000,000 bits = 1BTC)',
-      shortName: 'bits',
+      name: 'uBLK (1,000,000 uBLK = 1BLK)',
+	  shortName: 'uBLK',
       value: 100,
       decimals: 2,
       code: 'bit',
     },
     {
-      name: 'BTC',
-      shortName: 'BTC',
+      name: 'BLK',
+      shortName: 'BLK',
       value: 100000000,
       decimals: 8,
       code: 'btc',
